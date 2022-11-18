a8ab7e3c78893274c9583a1f1a0c2ff437b3c178
d0948112 <d0948112@o365.fcu.edu.tw>
2022-11-18 20:26:41 +0800

    First commit

---
+++ src/main/java/com/fcu/qaforum/QaforumApplication.java
@@ -0,0 +1,13 @@
+package com.fcu.qaforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaforumApplication.class, args);
+	}
+
+}
